0e58b9bfd81f689c78560b967d0f37b256f60d3d
Seif Abdelkader <saifm9633@gmail.com>
2025-12-13 18:32:15 +0200

    Add def sort_by_severity_desc and add import patient in node.py

--- sorting.py
+++ sorting.py
@@ -16,3 +16,22 @@ def sort_by_severity(linked_list):
                 swapped = True
            
             current = current.next
+
+def sort_by_severity_desc(linked_list):
+    if linked_list.head is None or linked_list.head.next is None:
+        return
+    
+    
+    swapped = True
+
+    while swapped:
+        swapped = False
+        current = linked_list.head
+
+        while current.next is not None:
+
+            if current.data.getSeverity() < current.next.data.getSeverity():
+                current.data, current.next.data = current.next.data, current.data
+                swapped = True
+           
+            current = current.next
